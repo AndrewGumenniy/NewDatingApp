48d14a1d63411104b27320549a842fd6bcad80b9
Andrew Gumenniy <Gumenniy.oik@gmail.com>
2020-02-16 21:17:24 +0200

    Auth Client

---
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  model: any = {};
+  @Output() cancelRegister = new EventEmitter();
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit() {
+  }
+
+  register() {
+    this.authService.register(this.model).subscribe(() => {
+      console.log('Register is successful');
+    });
+  }
+
+  cancel() {
+    this.cancelRegister.emit(false);
+    console.log('cancel');
+  }
+}
